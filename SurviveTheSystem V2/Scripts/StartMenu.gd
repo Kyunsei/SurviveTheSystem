1eb20ad50ad4fc688ffc4ca071ae1e21047771c7
icvara <icvara.barbier@gmail.com>
2024-05-05 13:38:59 +0100

    add starting menu

---
+++ SurviveTheSystem V2/Scripts/StartMenu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+'This Script deal with the Start Menu
+
+it define the different fonction of the buttons'
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+
