d589f65632d2f5d994d81fbc5eb81732953409ae
icvara <icvara.barbier@gmail.com>
2024-05-06 00:15:28 +0100

    add growth and multiplication on CPU

--- SurviveTheSystem V2/Scripts/Life.gd
+++ SurviveTheSystem V2/Scripts/Life.gd
@@ -6,14 +6,18 @@ var life_size_unit = 32
 var life_scene = load("res://Scenes/life.tscn") #load scene of block
 
 var parameters_array = [] 
+var state_array = [] 
 var world_matrix = []
 var par_number = 4 # Genome_ID, PV, Element, LifeCycle
 var Genome = {}
 
 func Init_matrix():
-	parameters_array.resize(World.world_size*World.world_size*par_number)
+	parameters_array.resize(World.world_size*life_size_unit*World.world_size*life_size_unit*par_number)
+	parameters_array.fill(-1)
 	world_matrix.resize(World.world_size*life_size_unit*World.world_size*life_size_unit)
 	world_matrix.fill(-1)
+	state_array.resize(World.world_size*life_size_unit*World.world_size*life_size_unit)
+	state_array.fill(-1)
 
 func Init_Parameter(INDEX,genome_index):
 	parameters_array[INDEX*par_number + 0] = genome_index #G_ID
@@ -25,7 +29,7 @@ func InstantiateLife(INDEX,folder):
 	var posIndex = world_matrix.find(INDEX)
 	var x = (posIndex % World.world_size) *life_size_unit
 	var y = (floor(posIndex/World.world_size))*life_size_unit
-
+	state_array[INDEX] = 1
 	var genome_index = parameters_array[INDEX*par_number + 0]
 	#if isOnScreen(Life.Life_Matrix_PositionX[index],Life.Life_Matrix_PositionY[index]):
 	if x >= 0 and y >= 0 and x < World.world_size*life_size_unit and y < World.world_size*life_size_unit :
@@ -36,18 +40,98 @@ func InstantiateLife(INDEX,folder):
 			folder.add_child(new_life)
 
 
+func LifeLoopCPU2():
+	#this function is the main loop for life entities, will be move to GPU
+	for l in range(parameters_array.size()/ par_number):
+		if parameters_array[l*par_number] != -1:
+			TakeElement(l)
+			if isGrowthing(l):
+				Growth(l)
+			else:
+				Duplicate(l)
+
+func LifeLoopCPU():
+	#this function is the main loop for life entities, will be move to GPU
+	var temp = state_array.duplicate()
+	var l = 0
+	while l != -1:
+		l = temp.find(1)
+		if parameters_array[l*par_number] != -1:
+			temp[l] += 1
+			TakeElement(l)
+			if isGrowthing(l):
+				Growth(l)
+			else:
+				Duplicate(l)
+
+
+
+func TakeElement(INDEX):
+	parameters_array[INDEX*par_number+2] += min(1,World.element)
+	World.element -= min(1,World.element)
+
+func isGrowthing(INDEX):
+	var output = false
+	var genome_index = parameters_array[INDEX*par_number+0]
+	var current_cycle = parameters_array[INDEX*par_number+3]
+	if current_cycle+1 <  Genome[genome_index]["lifecycle"].size():
+		if parameters_array[INDEX*par_number+2] >= Genome[genome_index]["lifecycle"][current_cycle+1]:
+			output = true
+	return output
+	
+func Growth(INDEX):
+	var genome_index = parameters_array[INDEX*par_number+0]
+	var current_cycle = parameters_array[INDEX*par_number+3]
+	parameters_array[INDEX*par_number+2] -= Genome[genome_index]["lifecycle"][current_cycle+1]
+	parameters_array[INDEX*par_number+3] += 1
+
 func Duplicate(INDEX):
-	pass
+	var genome_index = parameters_array[INDEX*par_number+0]
+	var current_cycle = parameters_array[INDEX*par_number+3]
+	var posIndex = world_matrix.find(INDEX)
+	var x = (posIndex % World.world_size) 
+	var y = (floor(posIndex/World.world_size))
+	if current_cycle+1 >=  Genome[genome_index]["lifecycle"].size():
+		if parameters_array[INDEX*par_number+2] >= Genome[genome_index]["lifecycle"][0]:
+			var newpos = PickRandomPlaceWithRange(x,y,3)
+			if world_matrix[newpos[0]*World.world_size + newpos[1]] == -1:
+				parameters_array[INDEX*par_number+2] -= Genome[genome_index]["lifecycle"][0]
+				BuildLife(newpos[0],newpos[1],genome_index)
+
+
+func BuildLife(x,y,genome_index):
+	var newindex = state_array.find(-1)
+	world_matrix[x*World.world_size + y] = newindex
+	Init_Parameter(newindex,genome_index)
+	state_array[newindex] = 0
+
+
+
+	
 	
 func RemoveInstance(INDEX):
 	pass
 	
+	
+func PickRandomPlace():
+	var rng = RandomNumberGenerator.new()
+	var random_x = rng.randi_range(0,World.worldsize*World.tile_size)
+	var random_y = rng.randi_range(0,World.worldsize*World.tile_size)
+	return [random_x, random_y]
+
+func PickRandomPlaceWithRange(centerx,centery,range):
+	var rng = RandomNumberGenerator.new()
+
+	var random_x = rng.randi_range(max(0,centerx-range),min(World.world_size*World.tile_size,centerx+range))
+	var random_y = rng.randi_range(max(0,centery-range),min(World.world_size*World.tile_size,centery+range))
+
+	return [random_x, random_y]
 
 func Init_Genome():
 	#This function create different life form
 	Genome[0] = {
 		"sprite" : [load("res://Art/grass_1.png"),load("res://Art/grass_2.png")],
-		"a" : 0
+		"lifecycle" : [2,4]
 	}
 	pass
 	
--- SurviveTheSystem V2/Scripts/main_scene.gd
+++ SurviveTheSystem V2/Scripts/main_scene.gd
@@ -10,10 +10,9 @@ func _ready():
 	for i in World.world_size:
 		for j in World.world_size:
 			World.InstantiateBlock(i,j,$Blocks)
-			if i == 5 and j == 5:
-				Life.world_matrix[i*World.world_size + j] = 0
-				Life.Init_Parameter(0,0)
-			if i == 1 and j == 5:
+			if i == int(World.world_size/2) and j == int(World.world_size/2):
+				Life.BuildLife(i,j,0)
+			'if i == 1 and j == 5:
 				Life.world_matrix[i*World.world_size + j] = 1
 				Life.Init_Parameter(1,0)
 			if i == 7 and j == 15:
@@ -21,18 +20,26 @@ func _ready():
 				Life.Init_Parameter(2,0)							
 			if i == 10 and j == 13:
 				Life.world_matrix[i*World.world_size + j] = 3
-				Life.Init_Parameter(3,0)	
-	Life.InstantiateLife(0,$Life)
-	Life.InstantiateLife(1,$Life)
-	Life.InstantiateLife(2,$Life)
-	Life.InstantiateLife(3,$Life)
+				Life.Init_Parameter(3,0)'	
+	#Life.InstantiateLife(0,$Life)
+	#Life.InstantiateLife(1,$Life)
+	#Life.InstantiateLife(2,$Life)
+	#Life.InstantiateLife(3,$Life)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$StarBackground.position = $Life/Player.position  #background follow player
 	$UI/FPS.text = "  " + str(Engine.get_frames_per_second()) + " FPS" #FPS
+	var idx = Life.state_array.find(0)
+	if idx >= 0:
+		Life.InstantiateLife(idx,$Life)
 	pass
 	
 	
+
+
+
+func _on_life_timer_timeout():
+	Life.LifeLoopCPU() 
 
