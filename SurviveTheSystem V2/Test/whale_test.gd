84e293b228a3ece95f0b8e94b4e2baa56e5d55b5
icvara <icvara.barbier@gmail.com>
2025-01-02 16:44:50 +0000

    add surprise on 5th day ;)

---
+++ SurviveTheSystem V2/Test/whale_test.gd
@@ -0,0 +1,29 @@
+extends Sprite2D
+
+var active = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = Vector2(-2500,-2500)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if World.day == 5:
+		whale_appear()
+	if active :
+		Whale_event(delta)
+
+
+func whale_appear():
+	active = true
+	position = Life.player.position - Vector2(2500,2500)
+	show()
+	$AudioStreamPlayer.playing = true
+	
+func Whale_event(delta):
+	position += Vector2(250,250) * delta
+
+func _input(event):
+	if event.is_action_pressed("interact"):
+			whale_appear()
