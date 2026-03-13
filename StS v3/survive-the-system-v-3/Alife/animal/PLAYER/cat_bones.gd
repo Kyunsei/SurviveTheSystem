e10b80cb2971dc16e4296277f157afcde6270ba8
Kyunsei <kirbyvaroujan@gmail.com>
2026-03-13 14:57:44 +0100

    cat bones no long expelled upstream, cat bones still able to go up
    cat bones now don't go on or below the player on player death.

--- StS v3/survive-the-system-v-3/Alife/animal/PLAYER/cat_bones.gd
+++ StS v3/survive-the-system-v-3/Alife/animal/PLAYER/cat_bones.gd
@@ -1,19 +1,24 @@
 extends RigidBody3D
-
+var target_position = Vector3(0,0,0)
+var moving = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	rotation_degrees.y = randf_range(0.0, 360.0)
 	linear_velocity = Vector3.ZERO
-		#$CollisionShape3D.disabled = true
-		#gravity_scale = 0
-		#mass = 0
-		#await get_tree().create_timer(0.5).timeout
-		#$CollisionShape3D.disabled = false
-		#gravity_scale = 1.0
-		#mass = 100.0
+	$CollisionShape3D.disabled = true
+	gravity_scale = 0
+	mass = 0
+	target_position = Vector3(global_position.x,0, global_position.z)
+	moving = true
+	await get_tree().create_timer(1.0).timeout
+	var moving = false
+	$CollisionShape3D.disabled = false
+	gravity_scale = 1.0
+	mass = 100.0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _process(delta) :
+	if global_position.y >= 0.0 and moving:
+		position = position.move_toward(target_position,8*delta)
