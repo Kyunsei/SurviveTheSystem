f985bebb96f225640d38fee2b484ad7c7a3ddec5
Kyunsei <kirbyvaroujan@gmail.com>
2026-03-14 18:09:05 +0100

    you're going to love this one

--- StS v3/survive-the-system-v-3/Alife/animal/PLAYER/player.gd
+++ StS v3/survive-the-system-v-3/Alife/animal/PLAYER/player.gd
@@ -196,7 +196,12 @@ func _physics_process(_delta: float) -> void:
 		velocity.x = direction.x *speed 
 		velocity.z = direction.z *speed 
 		#data_movement_to_server.rpc_id(1, global_position)
-
+		if velocity.x != 0 or velocity.z != 0:
+			if not $PlayerAnimater.is_playing() or $PlayerAnimater.current_animation != "leg_movement":
+				$PlayerAnimater.play("leg_movement")
+		else:
+			if $PlayerAnimater.current_animation == "leg_movement":
+				$PlayerAnimater.stop()
 		if direction != Vector3(0,0,0):
 			$MeshInstance3D.rotation.y = $camera_anchor.rotation.y 
 			#$Action_Area3D.rotation.y = $camera_anchor.rotation.y 
