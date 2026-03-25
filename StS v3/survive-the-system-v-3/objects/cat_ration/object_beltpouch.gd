a118b3092d8eb89e7175ee68b596534b80240069
Kyunsei <kirbyvaroujan@gmail.com>
2026-03-25 01:00:00 +0100

    beltpouch and timewarp can both be eaten and used

--- StS v3/survive-the-system-v-3/objects/cat_ration/object_beltpouch.gd
+++ StS v3/survive-the-system-v-3/objects/cat_ration/object_beltpouch.gd
@@ -19,7 +19,13 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 
 
 static func on_use(_player): 
-	pass
+		_player.get_node("Player_HUD").get_node("Inventory").remove_selected(int(_player.name))
+		var value = 0.5
+		_player.inventory_capacity_upgrade += value
+		_player.lifedata["Inventory_capacity"] = _player.inventory_capacity_upgrade
+		_player.show_label_above_player.rpc_id(int(_player.name),value, Color(1.0, 1.0, 1.7, 1.0), 3.0, ""," inventory capacity")
+		print("You equip the beltpouch")
+		pass
 
 static func on_use_secondary(_player, _state):
 	pass
--- StS v3/survive-the-system-v-3/objects/cat_ration/object_timewarp.gd
+++ StS v3/survive-the-system-v-3/objects/cat_ration/object_timewarp.gd
@@ -52,6 +52,33 @@ static func on_use(player):
 static func on_use_secondary(player, state):
 	pass
 	
-static func eat(_player):
-		pass
+static func eat(player):
+	var player_list = player.get_parent().player_array
+	var spaceship = player.get_parent().get_parent().get_node("SPACESHIP")
+	var spaceship_height = spaceship.global_position.y
+	var all_above = true
+	var mission_finished = false
+	for p in player_list:
+		if p.global_position.y < spaceship_height:
+			all_above = false
+			break
+		if p.finished_their_mission == true:
+			all_above = false
+			mission_finished = true
+			break
+	if all_above:
+		var inventory = player.get_node_or_null("Player_HUD/Inventory")
+		if inventory:
+			inventory.remove_selected(int(player.name))
+		var debug_ctrl = player.get_node_or_null("DEBUG_CONTROL")
+		if debug_ctrl:
+			debug_ctrl.change_server_simulation_speed2.rpc_id(1, 600)
+			for p in player_list:
+				p.show_label_above_player.rpc_id(int(p.name),1, Color(1.0, 1.0, 1.0, 1.0), 10.0,"Special" ,"Timewarp activated. Please wait")
+			#print("Timewarp activated: All players are above threshold.")
+	else:
+		if mission_finished:
+			player.show_label_above_player.rpc_id(int(player.name),1, Color(1.0, 1.0, 1.0, 1.0), 2.0,"Special","Timewarp unusable, you finished your mission!")
+		else:
+			player.show_label_above_player.rpc_id(int(player.name),1, Color(1.0, 1.0, 1.0, 1.0), 2.0,"Special","Timewarp failed: All players must be in the ship.")
 		
