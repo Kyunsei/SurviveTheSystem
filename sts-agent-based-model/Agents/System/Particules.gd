0da2e3fb3155888a3da2956e5a44562d4edf2af6
Aor <iib21@ic.ac.uk>
2026-04-17 13:07:17 +0100

    Some start on particule simulation

--- sts-agent-based-model/Agents/System/Particules.gd
+++ sts-agent-based-model/Agents/System/Particules.gd
@@ -1,6 +1,11 @@
 extends AgentSystem
 class_name PARTICULES_MOVE_SYSTEM
 
+@export var attraction_strength : float = 0
+@export var repulsion_strength : float = 0
+@export var rayon : float = 0
+
+
 
 func update(manager,delta):
 	pass
